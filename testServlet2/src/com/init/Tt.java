c7373d77e1bea3cf647358e795c65e1a45066be5
lemo <lemo@lemo-PC>
2017-10-23 13:09:10 +0800

    init

---
+++ testServlet2/src/com/init/Tt.java
@@ -0,0 +1,24 @@
+package com.init;
+
+
+import java.util.Set;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.annotation.HandlesTypes;
+
+@HandlesTypes({})
+public class Tt implements ServletContainerInitializer{
+
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext context)
+			throws ServletException {
+		// TODO Auto-generated method stub
+		ServletRegistration reg =context.addServlet("ss", "com.src.Test");
+		reg.addMapping("/surl");
+		
+	}
+
+}
